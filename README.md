94af01cc9710fb7c1f5e9ab7c16a6dc10a02be07
François Leurent <131.php@cloudyks.org>
2016-09-19 10:39:29 +0200

    Cleanup directory, abit

---
+++ README.md
@@ -0,0 +1,12 @@
+discify is a browserify plugin that generatee graph and stats of your bundle.
+
+# What you'll get
+* a /disc/ folder in your project root
+* a /disc/index.html standalone, neat, manually crafter repport HTML graph
+* a /disc/map.json file with also usefull informations
+
+
+# disc VS discify
+* Simplier workflow ; as discify is a plugin, it access live and raw from browserify and does not need a dedicated "useFullPath"
+
+  
--- index.js
+++ index.js
@@ -1,51 +1,46 @@
 "use strict";
 
-const merge = require('mout/object/merge');
-const fs   = require('fs');
-const untree = require('./untree');
-const graph = require('./graph');
+const fs     = require('fs');
+const path   = require('path');
 
-var combineSourceMap = require('combine-source-map');
+const merge            = require('mout/object/merge');
+const combineSourceMap = require('combine-source-map');
+const mkdirpSync       = require('nyks/fs/mkdirpSync');
+
+const untree = require('./untree');
+const graph  = require('./graph');
+
 
 module.exports = function(b) {
   var versions = {}, deps = {};
+
+  var outdir = mkdirpSync("disc");
 
   b.on("bundle", function(stream) {
 
     stream.once("end", function() {
       console.log("Wrote map");
-      fs.writeFileSync('map.json', JSON.stringify(versions, null, 2));
-      fs.writeFileSync('map2.json', JSON.stringify(deps, null, 2));
 
+      fs.writeFileSync(path.join(outdir, 'packages.json'), JSON.stringify(versions, null, 2));
+      fs.writeFileSync(path.join(outdir, 'browserify-deps.json'), JSON.stringify(deps, null, 2));
+
+      //deps contain un-processed module dependencies from browserify
       deps = untree(deps);
-      fs.writeFileSync('map3.json', JSON.stringify(deps, null, 2));
+      fs.writeFileSync(path.join(outdir, 'graph.json'), JSON.stringify(deps, null, 2));
 
-      graph(deps, function(err, body){
-        fs.writeFileSync('map.html', body);
+      graph(deps, function(err, body) {
+        fs.writeFileSync(path.join(outdir, 'map.html'), body);
       });
 
     });
   });
 
-
   b.on("dep", function(dep){
     var out = merge({}, dep);
-
     deps[out.id] = out;
     out.size = combineSourceMap.removeComments(out.source).length;
     delete out.source;
   });
-
-    b.on('transform', function (tr, mfile) {
-      console.log(mfile);
-/*
-        console.log(mfile);
-        tr.on('file', function (dep) {
-            watchFile(mfile, dep);
-        });
-*/
-    });
-
 
   b.pipeline.on('package', function (pkg) {
     if(!versions[pkg.name] )
