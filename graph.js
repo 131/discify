4f4c36919068d1bf928781c1d0d15c4bb0539c88
François Leurent <131.php@cloudyks.org>
2016-09-23 21:16:39 +0200

    Use browserify-wrap for prefix mgnt

--- graph.js
+++ graph.js
@@ -1,37 +1,36 @@
 "use strict";
 
-const fs     = require('fs');
-const concat = require('concat-stream');
+const fs      = require('fs');
+const path    = require('path');
+
+
+const concat     = require('concat-stream');
 const browserify = require('browserify');
-const path = require('path');
+const wrap       = require('browserify-wrap');
 
 var style    = fs.readFileSync(path.join(__dirname, 'html/theme.css'), 'utf-8');
 var template = fs.readFileSync(path.join(__dirname, 'html/index.html'), 'utf-8');
 
 
-function browser(chain) {
-  var b = browserify({
-    standalone : "discify"
-  });
-  b.add(path.join(__dirname, 'html/sequences.js'));
-  b.bundle().pipe(concat(function (body) {
-    chain(null, body);
-  }));
-}
-
 
 module.exports = function(data, chain){
 
-  browser(function(err, code){
-    var script = `${code}; discify( ${JSON.stringify(data)} , document.body);`;
+  var standalone = "discify";
 
-    var body  = template;
+  var b = browserify({ standalone });
+  b.add(path.join(__dirname, 'html/sequences.js'));
+  b.plugin(wrap, { suffix : `${standalone}( ${JSON.stringify(data)} , document.body);` });
 
+  b.bundle().pipe(concat(function (script) {
+
+    var body = template;
     body = body.replace(/&script;/, script);
     body = body.replace(/&style;/, style);
 
     chain(null, body);
-    
-  })
+  }));
+
+
+
 
 };
